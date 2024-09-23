c13f25b80964a115dd26e4b704aa15fa842232f4
Divy <divy1211.dc@gmail.com>
2024-09-23 20:17:28 +0800

    Fixed names

--- src/sections/sounds/sound.py
+++ src/sections/sounds/sound.py
@@ -3,24 +3,24 @@ from __future__ import annotations
 from binary_file_parser import BaseStruct, Retriever, Version
 from binary_file_parser.types import int16, uint16, int32
 
-from src.sections.sounds.sound_item import SoundItem
+from src.sections.sounds.sound_item import SoundFile
 
 
 class Sound(BaseStruct):
     @staticmethod
-    def set_sound_item_repeat(_, instance: Sound):
-        Retriever.set_repeat(Sound.sound_items, instance, instance.file_count)
+    def set_sound_file_repeat(_, instance: Sound):
+        Retriever.set_repeat(Sound.sound_files, instance, instance.sound_file_count)
 
     @staticmethod
-    def sync_sound_item_repeat(_, instance: Sound):
-        instance.file_count = len(instance.sound_items)
+    def sync_sound_file_repeat(_, instance: Sound):
+        instance.sound_file_count = len(instance.sound_files)
 
     # @formatter:off
     id: int                        = Retriever(int16,                                                           default = 0)
     play_delay: int                = Retriever(uint16,                                                          default = 0)
-    file_count: int                = Retriever(uint16,                                                          default = 0, on_set = [set_sound_item_repeat], on_write = [sync_sound_item_repeat])
+    sound_file_count: int          = Retriever(uint16,                                                          default = 0, on_set = [set_sound_file_repeat], on_write = [sync_sound_file_repeat])
     cache_time: int                = Retriever(int32,                                                           default = 300000)
     total_probability1: int        = Retriever(int16,     min_ver = Version((4, 5)), max_ver = Version((4, 5)), default = 100)
     total_probability2: int        = Retriever(int16,     min_ver = Version((7, 1)),                            default = 100)
-    sound_items: list[SoundItem]   = Retriever(SoundItem,                                                       default_factory = lambda _: [])
+    sound_files: list[SoundFile]   = Retriever(SoundFile,                                                       default_factory = lambda _: [])
     # @formatter:on
--- src/sections/sounds/sound_item.py
+++ src/sections/sounds/sound_item.py
@@ -4,7 +4,7 @@ from binary_file_parser import BaseStruct, Retriever, Version
 from binary_file_parser.types import uint16, str16, FixedLenStr, int32, int16
 
 
-class SoundItem(BaseStruct):
+class SoundFile(BaseStruct):
     # @formatter:off
     len_name_debug1: int        = Retriever(uint16,          min_ver = Version((4, 5)), max_ver = Version((4, 5)), default = 0)
     sound_name1: str            = Retriever(str16,           min_ver = Version((4, 5)), max_ver = Version((4, 5)), default = "")
